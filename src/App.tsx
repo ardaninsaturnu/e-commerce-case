7b75dfe904ea53544c7fda5726f836ffa901bb7e
hsmclk2505@gmail.com <hsmclk2505@gmail.com>
2022-10-09 14:01:39 +0300

    initialize has started. :tada: @main

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import Layout from './Layout';
 
 function App() {
   return (
-   <></>
+   <>
+    <Layout/>
+   </>
   );
 }
 
---
+++ src/Pages/ProductDetail/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ProductDetail = () => {
+    return(
+        <>Layout</>
+    )
+}
+
+export default ProductDetail;
